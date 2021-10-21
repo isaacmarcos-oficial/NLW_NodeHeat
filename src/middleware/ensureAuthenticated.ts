e6a3055365baea912d0f914f3c05cfe620bd6a38
Isaac Marcos <isaacmarcos@scdesign.com.br>
2021-10-20 21:40:42 -0300

    new version

--- src/middleware/ensureAuthenticated.ts
+++ src/middleware/ensureAuthenticated.ts
@@ -21,13 +21,14 @@ export function ensureAuthenticated (
     const [, token] = authToken.split(" ");
     
 try {
-    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload
+    const { sub } = verify(token, `$(process.env.JWT_SECRET)`) as IPayload;
 
     request.user_id = sub;
 
     return next();
 } catch (err) {
-    return response.status(401).json({ errorCode: "token.expired"});
-    
+    return response.status(401).json({
+        errorCode: "token expired",
+    });
     }
 }
