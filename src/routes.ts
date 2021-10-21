5453303e15738ea1c778dca47064a3ef54f69c40
Isaac Marcos <isaacmarcos@scdesign.com.br>
2021-10-20 22:49:29 -0300

    20 de outubro 22:49

--- src/routes.ts
+++ src/routes.ts
@@ -10,7 +10,7 @@ router.post("/authenticate", new AuthenticateUserController(). handle);
 router.post(
     "/messages",
     ensureAuthenticated,
-    new AuthenticateUserController().handle
+    new CreateMessageController().handle
     );
 
 export { router };
